a5c924280dcdccd101f88660d721f7a943cc0695
Rachel P <r4chelp@gmail.com>
2023-08-07 14:40:30 +0100

    temp_rh.py updated, temp, rh, and elapsed time added to data_queue

--- sensors/temp_rh.py
+++ sensors/temp_rh.py
@@ -1,40 +1,34 @@
 import RPi.GPIO as GPIO
-
-#pre-clean
-GPIO.cleanup()
-
-import RPi.GPIO as GPIO
 import time
 #first time, pip install adafruit-circuitpython-dht
 import adafruit_dht
 import board
 
+def DHT(DHT_pin, file_name, data_queue):
+    #Initialize DHT22 sensor
+    dht_sensor = adafruit_dht.DHT22(DHT_PIN, use_pulseio=False) 
 
-
-DHT_PIN = 23
-
-#Initialize DHT22 sensor
-dht_sensor = adafruit_dht.DHT22(board.D23, use_pulseio=False) 
-#dht_sensor = adafruit_dht.DHT22(DHT_PIN)
-
-while True:
-    try:
-        #Read temp and humidity from sensor
-        temp = dht_sensor.temperature
-        rh = dht_sensor.humidity
+    while True:
+        try:
+            #Read temp and humidity from sensor
+            temp = dht_sensor.temperature
+            rh = dht_sensor.humidity
+            elapsed_time = time.time() - start_time #calculate elapsed time
+            
+            #put data into the queue
+            data_queue.put((elapsed_time, temp, rh))
+            
+            with open(file_name, 'a') as file:
+                writer = csv.writer(file)
+                writer.writerow([elapsed_time, temp, rh])
         
-        #Print values to console
-        print(f"Temp: {temp:.1f} °C, Humidity: {rh:.1f}%")
-    
-    except RuntimeError as e:
-        #if there's an error reading data, print error message
-        print(f"Error reading data from DHT sensor: {e}")
-
-    # Wait for 10 seconds before reading data again
-    time.sleep(10)
-
-# Clean uip
-GPIO.cleanup()
-dht_sensor.exit()
-display.fill(0)
-display.show()
+            # Wait for 60 seconds before reading data again
+            time.sleep(60)
+        
+        except KeyboardInterrupt:
+            break
+            
+        except RuntimeError as e:
+            #if there's an error reading data, print error message
+            print(f"Error reading data from DHT sensor: {e}")
+            continue
