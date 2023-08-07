fc81bcebc03f8c7f442986cb84f1c25ef4193171
Rachel P <r4chelp@gmail.com>
2023-08-07 13:44:27 +0100

    main.py updated with lights function

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+
+import time
+import threading
+from LED_panels.py import lights
+
+
+
 # main file for the rPi beehaviour box. 
 # Setup duration of recordings directly from this file.
+
+if __name__ == "__main__":
+    # Set up pins and timing for components
+
+    #LED panels
+    R_LED_PIN = 20
+    W_LED_PIN = 21
+    LED_hrs = 12
+    LED_time = LED_hrs * 3600
+    
+    #Create threads for each task, pass relevant parameters
+    lights_thread = threading.Thread(target=lights, args=(R_LED_PIN, W_LED_PIN, LED_time,))
+    
+    #Start threads
+    lights_thread.start()
+    
+    #Wait for all threads to complete
+    lights_thread.join()
+    
+    print("All tasks completed")
+
 
 
 
