2b70a9865de023633073520fe27e1fc7798ed2b5
Rachel P <r4chelp@gmail.com>
2023-08-09 15:33:05 +0100

    OLED file debugged in rpi

--- OLED_display.py
+++ OLED_display.py
@@ -1,15 +1,15 @@
 # need to pip install adafruit-circuitpython-ssd1306
 # and sudo apt-get install python3-smbus
 
-import board
-import busio
-import adafruit_ssd1306
 import RPi.GPIO as GPIO
 import time
-import adafruit_dht
+import datetime
+import adafruit_ssd1306
+import busio
+import board
 import queue
 
-def OLED(data_queue, start_time, i2c):
+def OLED(data_queue, Rec_time, start_time, i2c):
     
     # Set the GPIO mode to BCM (Broadcom SOC channel numbering)
     GPIO.setmode(GPIO.BCM)
@@ -17,23 +17,40 @@ def OLED(data_queue, start_time, i2c):
     # Initialize I2C communication for the OLED display
     display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
     
-    while True: 
+    display.fill(0)
+    display.show()
+    
+    while time.time() - start_time < Rec_time: 
         try:
             temp, rh = data_queue.get()
-            elapsed_time = time.time() - start_time #calculate elapsed time
+            #elapsed_time = time.time() - start_time #calculate elapsed time
+            elapsed_time_s = time.time() - start_time
+            elapsed_time = datetime.timedelta(seconds=elapsed_time_s)
+            hours, remainder = divmod(elapsed_time.seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
+
+            elapsed_time_str = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
             
             #display data on OLED panel
             display.fill(0)
             display.text(f"{temp:.1f} C, {rh:.1f}% rh", 0, 0, 1)
-            display.text(f"REC time: {str(elapsed_time)}", 0, 24, 1)
+            display.text(f"REC time: {elapsed_time_str}", 0, 24, 1)
+            display.show()
+            
+        except KeyboardInterrupt:
+            break
+
+        # Wait for 10 seconds before reading data and updating the display again
+        time.sleep(2)
+    
+    while time.time() - start_time > Rec_time:
+        try:
+            # Display "recording finished!" and total Rec_time
+            display.fill(0)
+            display.text("Done!", 0, 0, 1)
+            display.text(f"Rec time: {elapsed_time_str}", 0, 24, 1)
             display.show()
             
         except KeyboardInterrupt:
             break
         
-        except RuntimeError as e:
-            # If there's an error reading data, print the error message
-            print(f"Error reading data from DHT sensor: {e}")
-
-        # Wait for 10 seconds before reading data and updating the display again
-        time.sleep(10)
