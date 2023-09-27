c64cd957c4beea6e70067aa9e0e40fb0f3c6d956
Rachel P <r4chelp@gmail.com>
2023-09-27 10:19:52 +0100

    buzz_LED.py outputs to JSON with updated filename

--- buzz_LED.py
+++ buzz_LED.py
@@ -9,27 +9,27 @@ import json
 def led_buzzer_control(duration, LED_BUZZER_PIN, buzz_length, buzz_space, buzz_file):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(LED_BUZZER_PIN, GPIO.OUT)
-    
+
     GPIO.output(LED_BUZZER_PIN, GPIO.LOW)
-    
+
     timestamps = [] # list to store timestamps
-    
+
     end_time = time.time() + duration
     while time.time() < end_time:
         current_timestamp = time.time()
         timestamps.append(current_timestamp)
-        
+
         GPIO.output(LED_BUZZER_PIN, GPIO.HIGH) #turn on
         time.sleep(buzz_length) #on for "buzz_length" time
         GPIO.output(LED_BUZZER_PIN, GPIO.LOW) # turn off
-        
+
         current_timestamp = time.time()
         timestamps.append(current_timestamp)
-        
+
         time.sleep(buzz_space-buzz_length) # space between buzzes
-    
+
     # save timestamps to a JSON file
-    with open(buzz_file, 'w') as file:
-        json.dump(timestamps, file)
-        
+    with open(buzz_file, 'w') as json_file:
+        json.dump(timestamps, json_file)
+
     GPIO.cleanup()
