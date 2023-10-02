a838ccc6889da5861edfe91489780b301950e5c5
Rachel P <r4chelp@gmail.com>
2023-10-02 16:49:52 +0100

    camera debugged to close properly at end of recording

--- pi_cam.py
+++ pi_cam.py
@@ -13,39 +13,56 @@ def cam(framerate, resolution, video_file, duration, start_time):
     # Initialize camera
     picam2 = Picamera2()
     
-    #Create video configuration
-    video_config = picam2.create_video_configuration({
-        "size": resolution,
-        "framerate": framerate
-    })
+    try:
+        #Create video configuration
+        video_config = picam2.create_video_configuration({
+            "size": resolution})
+            #"framerate": framerate
+            #})
     
-    #Configure the camera with the video config
-    picam2.configure(video_config)
+        #Configure the camera with the video config
+        picam2.configure(video_config)
     
-    #Initialize encoder
-    encoder = H264Encoder(bitrate=10000000)
+        #Initialize encoder
+        encoder = H264Encoder(bitrate=10000000)
 
-    # Calculate the end time
-    end_time = start_time + duration
+        # Calculate the end time
+        end_time = start_time + duration
     
-    # Create empty list to store timestamps
-    timestamps = []
+        # Initialize frame counter and timestamps list
+        frame_count = 0
+        timestamps = []
     
-    # Start recording
-    picam2.start_recording(encoder, video_file)
+        # Calculate time increment for each frame
+        frame_time_increment = 1.0 / framerate
     
-    while time.time() < end_time:
-        current_time = time.time()
-        timestamps.append(current_time)
-        time.sleep(1)
+        # Start recording
+        #picam2.start_preview(Preview.QTGL)
+        picam2.start_recording(encoder, video_file)
     
-    # stop recording
-    picam2.stop_recording()
+        while time.time() < end_time:
+            current_time = start_time + (frame_count * frame_time_increment)
+            timestamps.append(current_time)
+            frame_count += 1
+            time.sleep(frame_time_increment)
     
-    # Save timestamps to a text file 
-    with open(videop_file.replace('.h264', '_timestamps.txt'), 'w') as f:
-        for timestamp in timestamps:
-            f.write(f"{timestamp}\n")
-    # or as a JSON file
-    with open(video_file.replace('.h264', '_timestamps.json'), 'w') as f:
-        json.dump(timestamps, f)
+        # stop recording
+        picam2.stop_recording()
+        
+        picam2.close()
+    
+        # Save frame info to JSON file with timestamps
+        output_data = {
+            'frame_count': frame_count,
+            'timestamps': timestamps
+        }
+    
+        with open(video_file.replace('.h264', '_metadata.json'), 'w') as f:
+            json.dump(output_data, f)
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    finally:
+        # Close the camera
+        picam2.close()
