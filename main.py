4df68e22ceb408ddbaba5fb87306463f8fbe5009
Rachel P <r4chelp@gmail.com>
2024-01-30 13:00:14 +0000

    removed old .py threads

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ import sys
 import adafruit_ssd1306
 from picamera2 import Picamera2, Preview
 
-from LED_panels import lights
-from OLED_display import OLED, OLED_wipe
-from temp_rh import DHT
-from MEMs import MEMs
-from pi_cam import cam
-from buzz_LED import led_buzzer_control
-from button_wait import wait_for_button
-from kill_camera import kill_camera_processes, signal_handler
+from threads.LED_panels import lights
+from threads.OLED_display import OLED, OLED_wipe
+from threads.temp_rh import DHT
+from threads.pi_cam import cam
+from threads.buzz_LED import led_buzzer_control
+from threads.USB_mic import record_audio
+from threads.time_clapper import time_clapper
 
 # main file for the rPi beehaviour box. 
 # Setup duration of recordings directly from this file.
@@ -30,7 +29,10 @@ if __name__ == "__main__":
     #time.sleep(5)
     
     #Set Name
-    Name = "Behaviour Lab 01"
+    Name = "Buzz_Box_1"
+    
+    #Set rPi number:
+    rPi_num = 1
     #get date and start time
     current_time = time.localtime()
     day = time.strftime("%y%m%d", current_time)
@@ -38,100 +40,130 @@ if __name__ == "__main__":
     
     # Name folder on SSD to save files
     ssd_path = '/home/rPi1/myssd/'
-    day_folder = os.path.join(ssd_path, day)
+    #day_folder = os.path.join(ssd_path, day)
     
-    #Check if folder exists. If not, create it.
-    if not os.path.exists(day_folder):
-        os.makedirs(day_folder)
+    def create_incremental_subfolder(base_folder):
+        subfolders = [f for f in os.listdir(base_folder) if os.path.isdir(os.path.join(base_folder, f))]
         
-    # Set up pins and timing for components
+        if not subfolders:
+            new_subfolder = "001"
+        else:
+            last_subfolder = max(subfolders, key=lambda x: int(x))
+            new_subfolder = f"{int(last_subfolder) + 1:03d}"
+        
+        subfolder_path = os.path.join(base_folder, new_subfolder)
+        os.makedirs(subfolder_path)
+        return subfolder_path
     
+    if not os.path.exists(os.path.join(ssd_path, day)):
+        os.makedirs(os.path.join(ssd_path, day))
+    
+    ############################################
+        ## Set up PINS ##
+
     #Total recording time (24 hours = 86400s)
     #Rec_time = 24 * 60 * 60
-    Rec_time = 20 #one minute tester
+    Rec_time = 300 #recording segment time (s)
     
-    # Push button pin
-    BUTTON_PIN = 5 #GPIO5
     
-    #LED panels
-    R_LED_PIN = 20
-    W_LED_PIN = 21
-    LED_hrs = 12
-    LED_time = LED_hrs * 3600
+    ###############################
+        ## Loop to repeat threads ##
+        ############################
+    reps = 3
+    spaces = 3
     
-    LED_time = 5 #test at 30s per LED colour
     
-    # LED and Buzzer
-    BUZZER_PIN = 24
-    LED_PIN = 25
-    buzz_length = 0.5 # duration of buzz
-    buzz_space = 5 # time to wait until next buzz
-    buzz_file = os.path.join(day_folder, day + '_buzz.json')
+    for replicate in range(1, reps + 1):
+        
+        seg_time = time.time()
+        
+        GPIO.cleanup()
     
-    #DHT pin board.D23
-    #DHT_PIN = 'D23'
-    DHT_file = os.path.join(day_folder, day + '_DHT.json')
-    
-    #OLED display & MEMs
-    i2c = busio.I2C(board.SCL, board.SDA)
-    duration = Rec_time
-    audio_file = os.path.join(day_folder, day + '_audio.wav')
-    
-    #pi cam
-    resolution = (1280, 720)
-    framerate = 60
-    video_file = os.path.join(day_folder, day + '_video.h264')
+        day_folder = create_incremental_subfolder(os.path.join(ssd_path, day))
+        #LED panels
+        R_LED_PIN = 16
+        W_LED_PIN = 21
+        
+        # LED and Buzzer
+        BUZZER_PIN = 24
+        LED_PIN = 25
+        
+        #OLED display & MEMs
+        i2c = busio.I2C(board.SCL, board.SDA)
+        duration = Rec_time
+        
+        #pi cam
+        resolution = (1920, 1080) #max res with high framerate
+        framerate = 60 # comes out at ~47.9 fps
+        
+        #create thread-safe queue
+        data_queue = queue.Queue()
+        
+       #Wipe the OLED screen
+        GPIO.setmode(GPIO.BCM)
+        display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+        display.fill(0)
+        
+        OLED_wipe(Name, data_queue, i2c) 
+        # Filenames (as per day_folder)
+        buzz_file = os.path.join(day_folder, day + '_buzz.json')
+        DHT_file = os.path.join(day_folder, day + '_DHT.json')
+        audio_file = os.path.join(day_folder, day + '_audio.wav')
+        video_file = os.path.join(day_folder, day + '_video.h264')
 
-    #Initialize camera:
-    picam2 = Picamera2()
-    
-    #create thread-safe queue
-    data_queue = queue.Queue()
-    
-    #Wipe the OLED screen
-    GPIO.setmode(GPIO.BCM)
-    display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
-    display.fill(0)
-    
-    OLED_wipe(Name, data_queue, i2c)
-    
-    #Use button to initialize programme:
-    wait_for_button(BUTTON_PIN)
-    
-    #Create threads for each task, pass relevant parameters
-    lights_thread = threading.Thread(target=lights, args=(R_LED_PIN, W_LED_PIN, LED_time, start_time, Rec_time))
-    DHT_thread = threading.Thread(target=DHT, args=(DHT_file, data_queue, Rec_time, start_time))
-    OLED_thread = threading.Thread(target=OLED, args=(Name, data_queue, Rec_time, start_time, i2c))
-    #MEMs_thread = threading.Thread(target=MEMs, args=(data_queue, i2c, audio_file, duration, start_time))
-    cam_thread = threading.Thread(target=cam, args=(picam2, framerate, resolution, video_file, duration, start_time))
-    led_buzzer_thread = threading.Thread(target=led_buzzer_control, args=(duration, BUZZER_PIN, LED_PIN, buzz_length, buzz_space, buzz_file))
-    
-    #Start threads
-    lights_thread.start()
-    DHT_thread.start()
-    OLED_thread.start()
-    #MEMs_thread.start()
-    cam_thread.start()
-    led_buzzer_thread.start()
-    
-    # Wait for Rec_time (e.g., 24 hours), plus 15s buffer
-    time.sleep(Rec_time + 15)
-    
-    # join threads
-    print("Joining threads...")
-    
-    #Wait for all threads to complete
-    lights_thread.join()
-    DHT_thread.join()
-    OLED_thread.join()
-    #MEMs_thread.join()
-    cam_thread.join()
-    led_buzzer_thread.join()
-    
-    #Check to be sure camera is closed
-    picam2.close()
-    
-    print("All tasks completed")
+        #Initialize camera:
+        picam2 = Picamera2()
+        
+        #Wipe the OLED screen
+        GPIO.setmode(GPIO.BCM)
+        display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+        display.fill(0)
+        
+        OLED_wipe(Name, data_queue, i2c)
+        
+        
+        #Create threads for each task, pass relevant parameters
+        lights_thread = threading.Thread(target=lights, args=(R_LED_PIN, W_LED_PIN, Rec_time))
+        DHT_thread = threading.Thread(target=DHT, args=(DHT_file, data_queue, start_time, seg_time, Rec_time))
+        #OLED_thread = threading.Thread(target=OLED, args=(Name, data_queue, Rec_time, start_time, i2c))
+        USB_mic_thread = threading.Thread(target=record_audio, args=(duration, audio_file))
+        cam_thread = threading.Thread(target=cam, args=(picam2, framerate, resolution, video_file, duration, start_time))
+        led_buzzer_thread = threading.Thread(target=led_buzzer_control, args=(duration, BUZZER_PIN, LED_PIN, buzz_length, buzz_space, buzz_file))
+        time_clapper_thread = threading.Thread(target=time_clapper, args=(rPi_num, BUZZER_PIN, LED_PIN, Rec_time, buzz_file))
+        
+        #Start threads
+        lights_thread.start()
+        DHT_thread.start()
+        #OLED_thread.start()
+        cam_thread.start()
+        led_buzzer_thread.start()
+        USB_mic_thread.start()
+        
+        # Wait for Rec_time (e.g., 24 hours), plus 15s buffer
+        time.sleep(Rec_time) 
+        
+        # join threads
+        print("Joining threads...")
+        
+        #Wait for all threads to complete
+        lights_thread.join()
+        DHT_thread.join()
+        #OLED_thread.join()
+        cam_thread.join()
+        led_buzzer_thread.join()
+        USB_mic_thread.join()
+
+        #Check to be sure camera is closed
+        picam2.close()
+        
+        print("All tasks completed")
+        
+
+        if replicate < reps:
+            # sleep for specified duration between replicates
+            time.sleep(spaces)
+
 
 #clean up pins
 GPIO.cleanup()
+
