cb51060884a521367f703b53818090806f651614
RP <r4chelp@gmail.com>
2025-05-12 14:46:37 +0100

    added instructions for setting up BEEhaviour Labs on internet-disconnected rPis

--- rPi_program/threads/pi_cam.py
+++ rPi_program/threads/pi_cam.py
@@ -14,32 +14,8 @@ import json
 import subprocess
 
 def record_video(picam2, framerate, resolution, video_file, Rec_time):
-    
-    #setup timestamp 
-    hour = int(time.strftime('%H'))
-    now = time.strftime("%y_%m_%d_%H_%M")
-    colour = (255, 255, 255)
-    origin = (0,30)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    scale = 1
-    thickness = 2
-    
-    timestamps = []
-    
-    def apply_timestamp(request):
-        current_time = time.time()
-        milliseconds = (current_time - int(current_time)) * 1000
-        timestamp = time.strftime("%Y-%m-%d %X") + f".{int(milliseconds):03d}"
-        
-        # Append timestamp to list
-        timestamps.append(timestamp)
-        
-        with MappedArray(request, "main") as m:
-            cv2.putText(m.array, timestamp, origin, font, scale, colour, thickness)
-    
     try:
         #Create video configuration
-        picam2.pre_callback = apply_timestamp
         picam2.video_configuration.size = resolution
         picam2.video_configuration.controls.FrameRate = framerate
         picam2.video_configuration.controls.Saturation = 0
@@ -59,14 +35,6 @@ def record_video(picam2, framerate, resolution, video_file, Rec_time):
         
         picam2.close()
     
-        # Save frame info to JSON file with timestamps
-        output_data = {
-            'timestamps': timestamps
-            }
-    
-        with open(video_file.replace('.h264', 'frames_.json'), 'w') as f:
-            json.dump(output_data, f)
-    
     except Exception as e:
         print(f"An error occurred: {e}")
     
@@ -80,7 +48,7 @@ def convert_h264_to_mp4(h264_video_file):
     ffmpeg_vid_command = [
         'ffmpeg',
         '-i', h264_video_file,  # Input file
-        '-r', '45', # Framerate
+        '-r', '30', # Framerate
         '-c:v', 'libx264',  # Video codec
         '-crf', '28',  # CRF value for compression
         '-preset', 'fast',  # Preset for compression-efficiency tradeoff
